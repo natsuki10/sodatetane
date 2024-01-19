cedfd28e2db7ff95e807c7006f8b0064efe614c6
ujinatsu410 <natsuki@DESKTOP-NBIDN66.localdomain>
2024-01-19 16:52:36 +0900

    ログイン機能実装

---
+++ database/migrations/2024_01_19_065020_create__users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 50);
+            $table->string('email')->unique();
+            $table->string('password', 255);
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('_users');
+    }
+};
