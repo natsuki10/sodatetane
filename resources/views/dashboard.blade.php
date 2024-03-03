f8025bf9dc4a4d90fb88f9f24e8fa30b63c026ca
natsuki <natsuki.uji410@gmail.com>
2024-03-03 21:21:58 +0900

    トップページ実装#37

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <div class="ml-3">
+                        <h2 class="text-xl text-green-700 font-bold mb-4 ml-4">そだてたねとは？</h2>
+                        <div class="border-l-4 border-green-300 pl-2">
+                            <p>保育での制作遊びのアイディア探しに困っていませんか？</p>
+                            <p>制作遊びの試作の写真、手元にありませんか？</p>
+                            <p>そだてたねとは忙しい保育士さんのためのアイディア共有アプリです。</p>
+                        </div>
+                    </div>
+                    <div class="mt-6">
+                        <h2 class="text-xl text-green-700 font-bold mb-4 ml-7">ルール</h2>
+                        <div class="border-l-4 border-green-300 ml-3 pl-2">
+                            <h3 class="font-bold">--禁止事項--</h3>
+                            <p>・こどもの顔や名前が特定できる投稿</p>
+                            <p>・著作権侵害または知的財産権の侵害</p>
+                            <p>・ヘイトスピーチや差別</p>
+                            <p>・スパムや詐欺</p>
+                            <p>・違法行為の促進または描写</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
