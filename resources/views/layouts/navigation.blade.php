f83688d402eac97065ff95b9215a592d2d8c7064
natsuki <natsuki.uji410@gmail.com>
2024-03-03 18:59:49 +0900

    ハンバーガーメニュー実装#30

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('そだてたねとは') }}
                     </x-nav-link>
                     <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
                         投稿一覧
@@ -41,7 +41,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('アカウント設定') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -82,7 +82,19 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('そだてたねとは') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
+                {{ __('投稿一覧') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('posts.create')" :active="request()->routeIs('posts.create')">
+                {{ __('新規投稿') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('posts.my_posts')" :active="request()->routeIs('posts.my_posts')">
+                {{ __('マイ投稿') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('likes.index')" :active="request()->routeIs('likes.index')">
+                {{ __('いいねした投稿') }}
             </x-responsive-nav-link>
         </div>
 
@@ -95,7 +107,7 @@
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                    {{ __('アカウント設定') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
